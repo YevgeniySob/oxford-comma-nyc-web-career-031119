40aacce41ecb9da51e6dce8039cac40d1efefff2
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 05:02:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-  return array[0]
+  str = ""
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    str = array[0] + " and " + array[1]
+    return str
+  end
 end
